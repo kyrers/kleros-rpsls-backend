692ce6de343557f739872781bf3cf85f8daaa630
kyrers <kyrers.eth@gmail.com>
2025-01-22 22:12:18 +0000

    Server scaffold

---
+++ db.ts
@@ -0,0 +1,32 @@
+import { Pool } from "pg";
+
+let database: Pool;
+
+const connectDb = async () => {
+  try {
+    database = new Pool({
+      connectionString: process.env.DATABASE_CONNECTION_STRING,
+    });
+
+    database.on("error", (err: any) =>
+      console.log("### DATABASE CONNECT ERROR ###\n", err)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const closeConnection = async (_: any) => {
+  //Needs _ argument, otherwise it would be called after server start for some reason...
+  if (database) {
+    await database.end();
+  }
+
+  process.exit();
+};
+
+connectDb();
+process.once("SIGTERM", closeConnection);
+process.once("SIGINT", closeConnection);
+
+export { database };
---
+++ server.ts
@@ -0,0 +1,14 @@
+import express, { Express } from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app: Express = express();
+
+app.use(express.json());
+
+if (process.env.NODE_ENV !== "production") {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running locally on port ${PORT}`);
+  });
+}
